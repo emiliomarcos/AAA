23bf41aa100a4e858a3083ebf3f9ad8ea6a9d5ec
Emilio Marcos <emilio7marcos@gmail.com>
2023-08-23 14:08:31 -0600

    basic footer setup

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,6 +2,16 @@ import './Footer.css'
 
 export default function Footer() {
   return (
-    <p>footer</p>
+    <footer>
+      <a className="social-icon" href="https://www.twitter.com" target="_blank" rel="noreferrer">
+        <p>icon</p>
+      </a>
+      <a className="social-icon" href="https://www.instagram.com" target="_blank" rel="noreferrer">
+        <p>icon</p>
+      </a>
+      <a className="social-icon" href="https://www.linkedin.com" target="_blank" rel="noreferrer">
+        <p>icon</p>
+      </a>
+    </footer>
   )
 }
