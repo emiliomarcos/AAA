11fe95e6f7a94e919b9a854eb6fc9116abc5960e
Emilio Marcos <emilio7marcos@gmail.com>
2023-08-23 14:27:15 -0600

    add footer icon links

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,17 +1,21 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faInstagram, faXTwitter } from '@fortawesome/free-brands-svg-icons'
 import './Footer.css'
 
 export default function Footer() {
   return (
     <footer>
-      <a className="social-icon" href="https://www.twitter.com" target="_blank" rel="noreferrer">
-        <p>icon</p>
-      </a>
-      <a className="social-icon" href="https://www.instagram.com" target="_blank" rel="noreferrer">
-        <p>icon</p>
-      </a>
-      <a className="social-icon" href="https://www.linkedin.com" target="_blank" rel="noreferrer">
-        <p>icon</p>
-      </a>
+      <div className="social-links">
+        <a className="social-icon" href="https://www.linkedin.com" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} size="lg" />
+        </a>
+        <a className="social-icon" href="https://www.twitter.com" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faXTwitter} size="lg" />
+        </a>
+        <a className="social-icon" href="https://www.instagram.com" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faInstagram} size="lg" />
+        </a>
+      </div>
     </footer>
   )
 }
