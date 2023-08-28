5a2571eca33508eecf0e0c3c5835cf1492e82f1d
Emilio Marcos <emilio7marcos@gmail.com>
2023-08-28 13:49:29 -0600

    create slogan component

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,4 +1,5 @@
 export { default as Navbar } from './Navbar/Navbar'
+export { default as Slogan } from './Slogan/Slogan'
 export { default as Solutions } from './Solutions/Solutions'
 export { default as About } from './About/About'
 export { default as Contact } from './Contact/Contact'
