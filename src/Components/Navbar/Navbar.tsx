88dc6c6d9df31416e6cf5c9d358ccad9c4b56730
Emilio Marcos <emilio7marcos@gmail.com>
2023-08-29 14:37:37 -0600

    change calendly link

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav>
       <img className='navbar-logo' src={logo} alt='Autonomatico logo' />
-      <a href='https://calendly.com/emiliomarcos/30min' target='_blank' rel='noreferrer'>
+      <a href='https://calendly.com/emiliomarcos/autonomatico-demo' target='_blank' rel='noreferrer'>
         <button className='navbar-demo'>Agenda un Demo</button>
       </a>
     </nav>
